6e9a47d86dfc645e8fbfc7dd57c5627c14a996e8
Andrew Broshears <ambroshears@gmail.com>
2022-01-18 19:32:27 -0600

    serviceNote

---
+++ ElevenNote.Services/NoteService.cs
@@ -0,0 +1,59 @@
+﻿using ElevenNote.Data;
+using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Services
+{
+    public class NoteService
+    {
+        private readonly Guid _userId;
+
+        public NoteService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateNote(NoteCreate model)
+        {
+            var entity =
+                new Note()
+                {
+                    OwnerId = _userId,
+                    Title = model.Title,
+                    Content = model.Content,
+                    CreatedUtc = DateTimeOffset.Now
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Notes.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<NoteListItem> GetNotes()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Notes
+                    .Where(e => e.OwnerId == _userId)
+                    .Select(
+                        e =>
+                        new NoteListItem
+                        {
+                            NoteId = e.NotId,
+                            Title = e.Title,
+                            CreatedUtc = e.CreatedUtc
+                        }
+                        );
+                return query.ToArray();
+            }
+        }
+    }
+}
